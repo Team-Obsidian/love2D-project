bbeaadf3eadcfdbfdb330a9006159dcb5457f503
Team-Obsidian <kepler1400@gmail.com>
2024-03-15 21:40:40 -0500

    create movement collision, player and enemy

--- main.lua
+++ main.lua
@@ -18,7 +18,7 @@ function love.load()
    player.deltaY = 0
    player.width = 50
    player.height = 50
-   player.onGround = False
+   player.onGround = true
    print('hahaha')
 
    --going to depricate once testing is over
@@ -26,13 +26,13 @@ function love.load()
 
 
    enemy = {}
-   enemy.xPos = 200
+   enemy.xPos = 0
    enemy.deltaX = 0
-   enemy.yPos = 200
+   enemy.yPos = 400
    enemy.deltaY = 0
-   enemy.width = 50
+   enemy.width = 600
    enemy.height = 50
-   enemy.onGround = False
+   enemy.onGround = false
    enemy.speed = 100
 
 
@@ -41,8 +41,8 @@ end
 
 function makeWallRect(xPos, yPos, width, height)
     local rect = {}
-    rect.x = xPos or 0
-    rect.y = yPos or 0
+    rect.xPos = xPos or 0
+    rect.yPos = yPos or 0
     rect.width = width or 200
     rect.height = height or 100
     table.insert(wallObjects, rect)
@@ -86,85 +86,166 @@ function doesCollideRect(object1, object2)
         --print('start of doesCollideRect: '.. testVarNum)
         --testVarNum = testVarNum + 1
     --if x values intersect
-    if (object1.x + object1.width) > object2.x and (object1.x < object2.x + object2.width) then
+    if (object1.xPos + object1.width) > object2.xPos and (object1.xPos < object2.xPos + object2.width) then
         --print('object1 xCollide with object2')
         collides = collides + 1
     end
 
     --if y values intersect
-    if (object1.y + object1.height) > object2.y and (object1.y < object2.y + object2.height) then
+    if (object1.yPos + object1.height) > object2.yPos and (object1.yPos < object2.yPos + object2.height) then
         --print('object1 yCollide with object2')
         collides = collides + 1
     end
 
     --if both collided
     if collides == 2 then
-        --print('end of doesCollideRect, TRUE')
-        return True
+       -- print('end of doesCollideRect, TRUE')
+        return true
     else
         --print('end of doesCollideRect, FALSE')
-        return False
+        return false
     end
 
 end
 
---[[
-function willCollideSides(object1, object2)
+
+function willCollide(object1, object2)
     local collides = {}
         --print('start of doesCollideRect: '.. testVarNum)
         --testVarNum = testVarNum + 1
-    --if x values intersect
-    if (object1.x + object1.width) > object2.x and (object1.x < object2.x + object2.width) then
-        --print('object1 xCollide with object2')
-        collides = collides + 1
-    end
 
-    --if y values intersect
-    if (object1.y + object1.height) > object2.y and (object1.y < object2.y + object2.height) then
-        --print('object1 yCollide with object2')
-        collides = collides + 1
-    end
+    --check if object1 is at a vertical position to hit horizontally
+    if (object1.yPos + object1.height) > object2.yPos and (object1.yPos < object2.yPos + object2.height) then
 
-    --if both collided
-    if collides == 2 then
-        --print('end of doesCollideRect, TRUE')
-        return True
+
+        --if object1 left hits object2 right
+        if (object1.xPos + player.deltaX < object2.xPos + object2.width) then
+            collides.left = true
+        else
+            collides.left = false
+        end
+
+        --if object1 right hits object2 left
+        if (object1.xPos + object1.width + player.deltaX) > object2.xPos then
+            collides.right = true
+        else
+            collides.right = false
+        end
+
     else
-        --print('end of doesCollideRect, FALSE')
-        return False
+            collides.left = false
+            collides.right = false
     end
+
+    --check if object1 is at a horizontal position to hit vertically
+    if (object1.xPos + object1.width) > object2.xPos and (object1.xPos < object2.xPos + object2.width) then
+       
+        --if object1 top hits object2 bottom
+        if (object1.yPos + object1.deltaY) < object2.yPos + object2.height then
+            collides.top = true
+        else
+            collides.top = false
+        end
+
+        --if object1 bottom hits object2 top
+        if (object1.yPos + object1.height + object1.deltaY > object2.yPos) then
+            collides.bottom = true
+        else
+            collides.bottom = false
+        end
+
+    else
+            collides.top = false
+            collides.bottom = false
+    end
+
+    --collides should be {['left']=true,['right']=true,['top']=true,['bottom']=true} when inside of object 2
+    return collides
 end
---]]
+
 
 
 function love.update(dTime)
+
     if player.deltaX ~= 0 then
         player.deltaX = player.deltaX*0.80
    end
+
+
+   if player.onGround == false then
+        print("player not on ground")
+        player.deltaY = player.deltaY + 20*dTime
+    end
+
+
 
     if player.deltaY ~= 0 then
         player.deltaY = player.deltaY*0.80
    end
 
+
+--[[
+    if doesCollideRect(player, enemy) == true then
+        --print("player is on ground")
+        player.onGround = true
+    else
+    end
+--]]
+
+
+--extra syntax after the willCollide stuff is because willCollide only checks for if it's past the edge, not whether it's still within the object
     if love.keyboard.isDown('right') and math.abs(player.deltaX) < 150 then
         player.deltaX = player.deltaX + 50*dTime
     --else
     --    player.deltaX = player.deltaX - 10
+        if willCollide(player, enemy).right and player.xPos + player.width < (enemy.xPos + 5) then
+            player.deltaX = 0
+            player.xPos = enemy.xPos - player.width
+        end
     end
     if love.keyboard.isDown('left') and math.abs(player.deltaX) < 150 then
         player.deltaX = player.deltaX - 50*dTime
    --else
    --     player.deltaX = player.deltaX - 10
+
+        --if player is just right of the wall's right side and player is no more than 5 pixels into the object (which means boundary breaking can happen?) 
+        if willCollide(player, enemy).left and player.xPos > (enemy.xPos+enemy.width - 5) then
+            --player stops moving horizontally
+            player.deltaX = 0
+            --player snaps to wall
+            player.xPos = enemy.xPos + enemy.width
+        end
     end
+
+
+
 
     --will reconfigure to jump thing later, acts like top-down for now
-    if love.keyboard.isDown('down') and math.abs(player.deltaY) < 100 then
-        player.deltaY = player.deltaY + 50*dTime
+    if love.keyboard.isDown('down') then
+        if math.abs(player.deltaY) < 100 then
+            player.deltaY = player.deltaY + 50*dTime
+        end
+
+        if willCollide(player, enemy).bottom and player.yPos + player.height < (enemy.yPos + 5) then
+            player.deltaY = 0
+            player.yPos = enemy.yPos - player.height
+        end
     end
-    if love.keyboard.isDown('up') and math.abs(player.deltaY) < 100 then
+    if love.keyboard.isDown('up') then
+        if math.abs(player.deltaY) < 100 then
         player.deltaY = player.deltaY - 50*dTime
+        end
+
+        if willCollide(player, enemy).top and player.yPos > (enemy.yPos + enemy.height - 5) then
+            player.deltaY = 0
+            player.yPos = enemy.yPos + enemy.height
+        end
     end
 
+    if love.keyboard.isDown('space') and player.onGround then
+        player.deltaY = player.deltaY - 500*dTime
+        player.onGround = false
+    end
 -- bad version of movement without momentum, test collision only
 
 --[[
@@ -204,8 +285,16 @@ function love.update(dTime)
 
 
 
+
+
     player.xPos = player.xPos + player.deltaX
+
+    --if player.onGround == false then
     player.yPos = player.yPos + player.deltaY
+    --else
+     --   player.deltaY = 0
+    --end
+
 end
 
 function love.draw()
@@ -213,6 +302,7 @@ function love.draw()
    love.graphics.draw(image, imgx, imgy)
    love.graphics.print("Click and drag the cake around or use the arrow keys", 10, 10)
    --]]
+   --makeWallRect(0,400,400,200)
     for i,v in ipairs(wallObjects) do
         love.graphics.rectangle('line',v.xPos,v.yPos,v.width,v.height)
     end
@@ -258,8 +348,12 @@ function love.keypressed(key)
     end--]]
 
     if key=='t' then
-        doesCollideRect(player, enemy)
+        print('left is: '..tostring(willCollide(player,enemy).left).. ' right is: '..tostring(willCollide(player, enemy).right))
         --table.insert(wallObjects, setWallProp{xPos=400,yPos=0,width=130,height=100})
+    end
+
+    if key=='p' then
+        print('player.onGround is: '.. tostring(player.onGround))
     end
 
 end
