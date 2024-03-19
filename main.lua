5bcd9361878f0fdad1f308d90272bedf92b5272a
Team-Obsidian <kepler1400@gmail.com>
2024-03-19 11:54:13 -0500

    level maps and loading/unloading levels

--- main.lua
+++ main.lua
@@ -3,10 +3,9 @@ io.stdout:setvbuf("no")
 
 
 function love.load()
+    --love.window.setMode(800, 600, {resizable=false, vsync=0})
 
-    canLand = false
-
-    level = 1
+    level = {x=1,y=1}
     --[[
    image = love.graphics.newImage("cake.jpg")
    love.graphics.setNewFont(12)
@@ -19,7 +18,7 @@ function love.load()
 
    --gravity stuff (https://2dengine.com/doc/platformers.html)
 
-   jumpHeight = 150
+   jumpHeight = 800
    --generic number, needs deltaTime to work properly
    timeToApex = 60
 
@@ -32,6 +31,7 @@ function love.load()
    player.width = 50
    player.height = 50
    player.onGround = true
+   player.loop = false
 
 
    print('hahaha')
@@ -52,111 +52,129 @@ function love.load()
 ]]--
 
    wallObjects = {}
-end
 
-function makeWallRect(param)
-    local rect = {}
-    rect.xPos = param.xPos or 0
-    rect.yPos = param.yPos or 0
-    rect.width = param.width or 200
-    rect.height = param.height or 100
-    rect.level = param.level or 1
-    rect.returns = param.returns or false
-    if param.returns then
-        return rect
-    else
-        table.insert(wallObjects, rect)
-    end
-    --return rectangle instead of automatically inserting it into current level objects
-    --return rect
-end
-
-
-
-function doesCollideRect(object1, object2)
-    local collides = 0
-        --print('start of doesCollideRect: '.. testVarNum)
-        --testVarNum = testVarNum + 1
-    --if x values intersect
-    if (object1.xPos + object1.width) > object2.xPos and (object1.xPos < object2.xPos + object2.width) then
-        --print('object1 xCollide with object2')
-        collides = collides + 1
-    end
-
-    --if y values intersect
-    if (object1.yPos + object1.height) > object2.yPos and (object1.yPos < object2.yPos + object2.height) then
-        --print('object1 yCollide with object2')
-        collides = collides + 1
-    end
-
-    --if both collided
-    if collides == 2 then
-       -- print('end of doesCollideRect, TRUE')
-        return true
-    else
-        --print('end of doesCollideRect, FALSE')
-        return false
-    end
-
-end
-
-
-function willCollide(object1, object2)
-
-
-    local collides = {}
-        --print('start of doesCollideRect: '.. testVarNum)
-        --testVarNum = testVarNum + 1
-
-    --check if object1 is at a vertical position to hit horizontally
-    if (object1.yPos + object1.height) > object2.yPos and (object1.yPos < object2.yPos + object2.height) then
-
-
-        --if object1 left passes object2 right and object1 right is still right of object2 right
-        if (object1.xPos + player.deltaX < object2.xPos + object2.width) and object1.xPos + object1.width > object2.xPos + object2.width - wallBuffer then
-            collides.left = true
+       function makeWallRect(param)
+        local rect = {}
+        rect.xPos = param.xPos or 0
+        rect.yPos = param.yPos or 0
+        rect.width = param.width or 200
+        rect.height = param.height or 100
+        rect.level = param.level or 1
+        rect.returns = param.returns or false
+        if param.returns then
+            return rect
         else
-            collides.left = false
+            table.insert(wallObjects, rect)
+        end
+        --return rectangle instead of automatically inserting it into current level objects
+        --return rect
+    end
+
+    function loadLevel(levelNum)
+        wallObjects = {}
+        if levelNum.y == 1 then
+            if levelNum.x == 1 then
+                makeWallRect{xPos=-50,yPos=400,width=900,height=300}
+            elseif levelNum.x == 2 then
+                makeWallRect{xPos=-50,yPos=400,width=200,height=300}
+                makeWallRect{xPos=500,yPos=300,width=350,height= 100}
+            end
+        else
+            level.x = 1
+            level.y = 1
+            loadLevel(level)
+        end
+    end
+
+    function doesCollideRect(object1, object2)
+        local collides = 0
+            --print('start of doesCollideRect: '.. testVarNum)
+            --testVarNum = testVarNum + 1
+        --if x values intersect
+        if (object1.xPos + object1.width) > object2.xPos and (object1.xPos < object2.xPos + object2.width) then
+            --print('object1 xCollide with object2')
+            collides = collides + 1
         end
 
-        --if object1 right passes object2 left and object1 left is still left of object2 right
-        if (object1.xPos + object1.width + player.deltaX) > object2.xPos and object1.xPos < object2.xPos + wallBuffer then
-            collides.right = true
-        else
-            collides.right = false
+        --if y values intersect
+        if (object1.yPos + object1.height) > object2.yPos and (object1.yPos < object2.yPos + object2.height) then
+            --print('object1 yCollide with object2')
+            collides = collides + 1
         end
 
-    else
-            collides.left = false
-            collides.right = false
+        --if both collided
+        if collides == 2 then
+           -- print('end of doesCollideRect, TRUE')
+            return true
+        else
+            --print('end of doesCollideRect, FALSE')
+            return false
+        end
+
     end
 
-    --check if object1 is at a horizontal position to hit vertically
-    if (object1.xPos + object1.width) > object2.xPos and (object1.xPos < object2.xPos + object2.width) then
-       
-        --if object1 top hits object2 bottom
 
-        if (object1.yPos + object1.deltaY) < object2.yPos + object2.height and object1.yPos + object1.height > object2.yPos + object2.height - wallBuffer then
-            collides.top = true
+    function willCollide(object1, object2)
+
+
+        local collides = {}
+            --print('start of doesCollideRect: '.. testVarNum)
+            --testVarNum = testVarNum + 1
+
+        --check if object1 is at a vertical position to hit horizontally
+        if (object1.yPos + object1.height) > object2.yPos and (object1.yPos < object2.yPos + object2.height) then
+
+
+            --if object1 left passes object2 right and object1 right is still right of object2 right
+            if (object1.xPos + player.deltaX < object2.xPos + object2.width) and object1.xPos + object1.width > object2.xPos + object2.width - wallBuffer then
+                collides.left = true
+            else
+                collides.left = false
+            end
+
+            --if object1 right passes object2 left and object1 left is still left of object2 right
+            if (object1.xPos + object1.width + player.deltaX) > object2.xPos and object1.xPos < object2.xPos + wallBuffer then
+                collides.right = true
+            else
+                collides.right = false
+            end
+
         else
-            collides.top = false
+                collides.left = false
+                collides.right = false
         end
 
-        --if object1 bottom hits object2 top
-        if object1.yPos + object1.height + object1.deltaY > object2.yPos and object1.yPos < object2.yPos + wallBuffer  then
-            collides.bottom = true
+        --check if object1 is at a horizontal position to hit vertically
+        if (object1.xPos + object1.width) > object2.xPos and (object1.xPos < object2.xPos + object2.width) then
+           
+            --if object1 top hits object2 bottom
+
+            if (object1.yPos + object1.deltaY) < object2.yPos + object2.height and object1.yPos + object1.height > object2.yPos + object2.height - wallBuffer then
+                collides.top = true
+            else
+                collides.top = false
+            end
+
+            --if object1 bottom hits object2 top
+            if object1.yPos + object1.height + object1.deltaY > object2.yPos and object1.yPos < object2.yPos + wallBuffer  then
+                collides.bottom = true
+            else
+                collides.bottom = false
+            end
+
         else
-            collides.bottom = false
+                collides.top = false
+                collides.bottom = false
         end
 
-    else
-            collides.top = false
-            collides.bottom = false
+        --collides should be {['left']=true,['right']=true,['top']=true,['bottom']=true} when inside of object 2
+        return collides
     end
 
-    --collides should be {['left']=true,['right']=true,['top']=true,['bottom']=true} when inside of object 2
-    return collides
+    loadLevel(level)
 end
+
+
 
 
 
@@ -192,8 +210,8 @@ function love.update(dTime)
 
 
 --extra syntax after the willCollide stuff is because willCollide only checks for if it's past the edge, not whether it's still within the object
+    local canLand = false
     
-    canLand = false
     for key, value in pairs(wallObjects) do    
         if willCollide(player, value).right then
             player.deltaX = 0
@@ -211,9 +229,8 @@ function love.update(dTime)
             player.deltaY = 0
             player.yPos = value.yPos - player.height
             canLand = true
-
         end
-        --print('player.onGround: '..tostring(canLand))
+
         if willCollide(player, value).top then
             player.deltaY = 0
             player.yPos = value.yPos + value.height
@@ -221,11 +238,12 @@ function love.update(dTime)
 
 
     end
-    if player.deltaY == 0 then
-        player.onGround = true
-    end
-    --player.onGround = canLand
 
+    if canLand then
+        player.onGround = true
+    else
+        player.onGround = false
+    end
 
     if love.keyboard.isDown('right') and math.abs(player.deltaX) < 150 then
     player.deltaX = player.deltaX + 50*dTime
@@ -245,7 +263,7 @@ function love.update(dTime)
 
 
     --will reconfigure to jump thing later, acts like top-down for now
-    if love.keyboard.isDown('down') and not player.onGround then
+    if love.keyboard.isDown('down') and player.deltaY == 0 and not player.onGround then
         if math.abs(player.deltaY) < 100 then
             player.deltaY = player.deltaY + 50*dTime
         end
@@ -257,8 +275,8 @@ function love.update(dTime)
         end
     end
 
-    if love.keyboard.isDown('space') then
-        player.deltaY = -1000*dTime
+    if love.keyboard.isDown('space') and player.onGround then
+        player.deltaY = -jumpHeight*dTime
         player.onGround = false
     end
 -- bad version of movement without momentum, test collision only
@@ -302,18 +320,30 @@ function love.update(dTime)
     --end
 
     --make player loop around to top after hitting bottom
-    if player.yPos > 800 and player.deltaY > 0 then
+    if player.yPos > 600 and player.deltaY > 0 then
+        if not player.loop then    
+            level.y = level.y - 1
+            loadLevel(level)
+        end
         player.yPos = -player.height
     end
     if player.xPos > 800 and player.deltaX > 0 then
         player.xPos = -player.width
+        level.x = level.x + 1
+        loadLevel(level)
     end
 
     if player.yPos + player.height < 0 and player.deltaY < 0 then
-        player.yPos = 800
+        if not player.loop then
+            level.y = level.y + 1
+            loadLevel(level)
+        end
+        player.yPos = 600
     end
     if player.xPos + player.width < 0 and player.deltaX < 0 then
         player.xPos = 800
+        level.x = level.x - 1
+        loadLevel(level)
     end
 
 end
@@ -331,7 +361,7 @@ function love.draw()
     love.graphics.setColor(0.8, 0.1, 0, 0.5)
     love.graphics.rectangle("fill", player.xPos, player.yPos, player.width, player.height)
 
-    love.graphics.setColor(0.1, 0, 0.8, 0.5)
+    love.graphics.setColor(0.1, 0, 0.8, 0.8)
 end
 
 
@@ -366,9 +396,9 @@ function love.keypressed(key)
     --[[if key == 'right' then
         x = x + 5
     end--]]
-
-
-
+    if key == 'l' then
+        player.loop = not player.loop
+    end
 
 end
 
@@ -381,7 +411,7 @@ function love.keyreleased(key)
    end
    --]]
     if key == 't' then
-        print('player.onGround: '..tostring(canLand))
+        print('player.onGround: '..tostring(player.onGround))
     end
 end
 
