d8a449de2dacc746be25727422052f29eacc348a
Team-Obsidian <78922515+Team-Obsidian@users.noreply.github.com>
2024-05-24 17:26:18 -0500

    basic test

--- main.lua
+++ main.lua
@@ -1,15 +1,35 @@
 --let's get to work!
 io.stdout:setvbuf("no")
-winX = 1280
-winY = 720
+winX = 320
+winY = 180
 
 
-player = {}
 
-function genPlayer(q)
-	local temp = {}
 
-	temp.xPos = q.xPos or
+function love.load()
 
+
+	function genPlayer(q)
+	
+		local temp = {}
+
+		temp.xPos = q.xPos or winX/2
+		temp.yPos = q.yPos or winY/2
+		temp.width = q.width or 12
+		temp.height = q.height or 16
+
+
+	end
+	player = genPlayer{}
+
+end
+
+
+
+
+
+function love.graphics.draw()
+	love.graphics.setColor(0.8, 0.2, 0.8, 1)
+	love.graphics.Rectangle(player.xPos,player.yPos,player.width,player,height)
 
 end
